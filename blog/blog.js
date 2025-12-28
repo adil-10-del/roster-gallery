47802ce37b8fccea6f73271b8b6f906060e62f02
rostergallery <muhamadadilatu@gmail.com>
2025-12-29 06:49:33 +0700

    Update blog.js

--- blog/blog.js
+++ blog/blog.js
@@ -1,1 +1,35 @@
+// Topik Pilihan
+const topikWrap = document.getElementById("topik-pilihan");
+BLOG_DATA.topikPilihan.forEach(t => {
+  topikWrap.innerHTML += `
+    <a href="${t.url}" class="topik-card">${t.title}</a>
+  `;
+});
+
+// Artikel
+const artikelWrap = document.getElementById("artikel-list");
+BLOG_DATA.artikel.forEach(a => {
+  artikelWrap.innerHTML += `
+    <article class="artikel-item">
+      <img src="${a.image}" alt="${a.title}">
+      <div>
+        <h3><a href="${a.url}">${a.title}</a></h3>
+        <p class="meta">${a.kategori} • ${a.tanggal}</p>
+        <p>${a.ringkas}</p>
+      </div>
+    </article>
+  `;
+});
+
+// Paling Dicari
+const dicariWrap = document.getElementById("paling-dicari");
+BLOG_DATA.palingDicari.forEach(d => {
+  dicariWrap.innerHTML += `<li><a href="#">${d}</a></li>`;
+});
+
+// Kategori
+const kategoriWrap = document.getElementById("kategori-list");
+BLOG_DATA.kategori.forEach(k => {
+  kategoriWrap.innerHTML += `<li><a href="#">${k}</a></li>`;
+});
 
