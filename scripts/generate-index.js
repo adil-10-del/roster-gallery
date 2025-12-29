041e83333fb4b2975695f6b2fe61bac05808ddda
rostergallery <muhamadadilatu@gmail.com>
2025-12-29 07:27:55 +0700

    Update and rename update-index-category.js to generate-index.js

---
+++ scripts/generate-index.js
@@ -0,0 +1,21 @@
+import fs from "fs";
+
+const postsDir = "./blog/posts";
+const files = fs.readdirSync(postsDir);
+
+const data = files.map(file => {
+  return {
+    title: file.replace(/-/g, " ").replace(".html", ""),
+    url: `/blog/posts/${file}`,
+    kategori: "Artikel Produk",
+    tanggal: new Date().toISOString().split("T")[0],
+    ringkas: "Artikel seputar produk konstruksi Roster Gallery.",
+    image: "/assets/images/placeholder.jpg"
+  };
+});
+
+fs.writeFileSync(
+  "./blog/blog-data.json",
+  JSON.stringify({ artikel: data }, null, 2)
+);
+
