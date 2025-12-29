1b574d673df536f4afbbe8e68605b08c1af14635
rostergallery <muhamadadilatu@gmail.com>
2025-12-30 04:43:52 +0700

    Update blog.js

--- js/blog.js
+++ js/blog.js
@@ -1,32 +1,42 @@
 const blogData = [
   {
+    id: 1,
     title: "Keunggulan Roster Beton untuk Bangunan Modern",
     desc: "Roster beton memberikan sirkulasi udara dan estetika pada bangunan modern.",
     image: "assets/images/blog/roster.jpg",
+    category: "Roster Beton",
     link: "blog-detail.html?id=1"
   },
   {
+    id: 2,
     title: "Tips Memilih Paving Block Berkualitas",
     desc: "Kenali jenis paving block terbaik untuk halaman rumah dan proyek besar.",
     image: "assets/images/blog/paving.jpg",
+    category: "Paving Block",
     link: "blog-detail.html?id=2"
   },
   {
+    id: 3,
     title: "Perbedaan Bata Tempel dan Bata Ekspos",
     desc: "Mana yang lebih cocok untuk desain bangunan Anda?",
     image: "assets/images/blog/bata.jpg",
+    category: "Bata",
     link: "blog-detail.html?id=3"
   },
   {
+    id: 4,
     title: "Genteng Beton vs Genteng Tanah Liat",
     desc: "Bandingkan kelebihan dan kekurangannya.",
     image: "assets/images/blog/genteng.jpg",
+    category: "Genteng",
     link: "blog-detail.html?id=4"
   },
   {
+    id: 5,
     title: "Tips Memilih Material Bangunan Berkualitas",
     desc: "Panduan penting sebelum membeli bahan bangunan.",
     image: "assets/images/blog/material.jpg",
+    category: "General",
     link: "blog-detail.html?id=5"
   }
 ];
