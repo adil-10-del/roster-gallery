8a957ec5b098b4cb16cd4ab520ac5b6249f03d32
rostergallery <muhamadadilatu@gmail.com>
2026-01-01 07:24:51 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -13,3 +13,21 @@ window.addEventListener("scroll", () => {
   const nav = document.querySelector(".navbar");
   nav.classList.toggle("scrolled", window.scrollY > 50);
 });
+// HERO SLIDER AUTO
+const slides = document.querySelectorAll(".slide");
+let currentSlide = 0;
+
+function showSlide(index) {
+  slides.forEach((slide, i) => {
+    slide.classList.remove("active");
+    if (i === index) {
+      slide.classList.add("active");
+    }
+  });
+}
+
+setInterval(() => {
+  currentSlide++;
+  if (currentSlide >= slides.length) currentSlide = 0;
+  showSlide(currentSlide);
+}, 4500);
