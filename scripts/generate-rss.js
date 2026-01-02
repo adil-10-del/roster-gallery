8a8b796e6a46e7dcd1bd4493cf74aa99f4fde6c5
rostergallery <muhamadadilatu@gmail.com>
2026-01-03 03:44:50 +0700

    Update generate-rss.js

--- scripts/generate-rss.js
+++ scripts/generate-rss.js
@@ -1,42 +1,22 @@
 const fs = require("fs");
-const path = require("path");
 
-const ROOT = process.cwd();
-const blogPath = path.join(ROOT, "data/blog.json");
-const rssPath = path.join(ROOT, "rss.xml");
-
-const SITE_URL = "https://adil-10-del.github.io/roster-gallery";
-const SITE_TITLE = "Roster Gallery Blog";
-const SITE_DESC = "Artikel dan edukasi material bangunan: roster beton, paving block, genteng, bata, dan tips konstruksi.";
-
-if (!fs.existsSync(blogPath)) {
-  console.log("blog.json belum ada");
-  process.exit(0);
-}
-
-const blog = JSON.parse(fs.readFileSync(blogPath, "utf8"));
-
-const items = blog.posts.slice(0, 20).map(post => `
-  <item>
-    <title><![CDATA[${post.title}]]></title>
-    <link>${SITE_URL}/blog/${post.slug}.html</link>
-    <guid>${SITE_URL}/blog/${post.slug}.html</guid>
-    <pubDate>${new Date(post.date).toUTCString()}</pubDate>
-    <description><![CDATA[${post.excerpt}]]></description>
-  </item>
-`).join("");
-
-const rss = `<?xml version="1.0" encoding="UTF-8" ?>
-<rss version="2.0">
-<channel>
-  <title>${SITE_TITLE}</title>
-  <link>${SITE_URL}</link>
-  <description>${SITE_DESC}</description>
-  <language>id-ID</language>
-  ${items}
-</channel>
-</rss>
+const blog = JSON.parse(fs.readFileSync("data/blog.json"));
+let rss = `<?xml version="1.0"?>
+<rss version="2.0"><channel>
+<title>Roster Gallery Blog</title>
+<link>https://adil-10-del.github.io/roster-gallery/</link>
 `;
 
-fs.writeFileSync(rssPath, rss.trim());
-console.log("RSS feed dibuat");
+blog.posts.forEach(p => {
+  rss += `
+  <item>
+    <title>${p.title}</title>
+    <link>https://adil-10-del.github.io/roster-gallery/blog/${p.slug}.html</link>
+    <pubDate>${p.date}</pubDate>
+  </item>`;
+});
+
+rss += "</channel></rss>";
+fs.writeFileSync("rss.xml", rss);
+console.log("✅ RSS updated");
+
